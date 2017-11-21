7378847a350739a8060b3df87d4e2d561c808bad
Ali Chegini <ali.c.engineer@gmail.com>
2017-11-21 13:08:47 +0100

    added sounds for answers

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -17,8 +17,12 @@ class ViewController: UIViewController {
     var questionsAsked = 0
     var correctQuestions = 0
     var indexOfSelectedQuestion: Int = 0
+    let delayTime = 2   // seconds
     
     var gameSound: SystemSoundID = 0
+    
+    var correctSound: SystemSoundID = 1
+    var wrongSound: SystemSoundID = 2
     
     
     
@@ -33,6 +37,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadGameStartSound()
+        loadAnswerSounds()
         // Start game
         playGameStartSound()
         displayQuestion()
@@ -80,18 +85,32 @@ class ViewController: UIViewController {
         if sender.title(for: .normal) == correctAnswer {
             correctQuestions += 1
             questionField.text = "Correct!"
+            playCorrectAnswerSound()
             // Remove the question from the array so each question shows up once
             trivia.remove(at: indexOfSelectedQuestion)
         } else {
             questionField.text = "Sorry, wrong answer!"
+            playWrongAnswerSound()
             trivia.remove(at: indexOfSelectedQuestion)
             
             // Change the background of correct answer for extra credit
             //sender.tintColor = UIColor.green
             
+            switch correctAnswer {
+            case option1.currentTitle!:
+                option1.backgroundColor = UIColor.red
+            case option2.currentTitle!:
+                option2.backgroundColor = UIColor.red
+            case option3.currentTitle!:
+                option3.backgroundColor = UIColor.red
+            case option4.currentTitle!:
+                option4.backgroundColor = UIColor.red
+            default: print("")
+            }
+            
         }
         
-        loadNextRoundWithDelay(seconds: 2)
+        loadNextRoundWithDelay(seconds: delayTime)
     }
     
     func nextRound() {
@@ -119,7 +138,6 @@ class ViewController: UIViewController {
     }
     
 
-    
     // MARK: Helper Methods
     
     func loadNextRoundWithDelay(seconds: Int) {
@@ -143,5 +161,26 @@ class ViewController: UIViewController {
     func playGameStartSound() {
         AudioServicesPlaySystemSound(gameSound)
     }
+    
+    
+    // Helper functions to load and play sounds
+    func loadAnswerSounds() {
+        let pathToCorrectSoundFile = Bundle.main.path(forResource: "Correct", ofType: "wav")
+        let correctSoundURL = URL(fileURLWithPath: pathToCorrectSoundFile!)
+        AudioServicesCreateSystemSoundID(correctSoundURL as CFURL, &correctSound)
+        
+        let pathToWrongSoundFile = Bundle.main.path(forResource: "Fail", ofType: "wav")
+        let wrongSoundURL = URL(fileURLWithPath: pathToWrongSoundFile!)
+        AudioServicesCreateSystemSoundID(wrongSoundURL as CFURL, &wrongSound)
+    }
+    
+    func playCorrectAnswerSound() {
+        AudioServicesPlaySystemSound(correctSound)
+    }
+    
+    func playWrongAnswerSound() {
+        AudioServicesPlaySystemSound(wrongSound)
+    }
+    
 }
 
